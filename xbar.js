2fcaa6ca966c57ade2209148f55e9c1a6a48b5d9
Jeremy Johnson <j@point-free.org>
2013-08-24 01:13:45 -0700

    Changes to keep up with new object references in xbar

--- xbar.js
+++ xbar.js
@@ -1,9 +1,37 @@
-function xbar(obj, spec, head_str, head_obj, complements, adjuncts) {
-    var head = (head_str != undefined && obj[head_str] != undefined) ? head_str : head_obj;
+function xbar(obj, spec, spec_args, head, complements, adjuncts, adjunct_args) {
+    function is_string(s) {
+        return s && (typeof s == "string" || s instanceof String);
+    };
+    if (is_string(spec)) {
+        spec = obj[spec];
+
+        if (spec_args) {
+            spec = spec.apply(obj, spec_args);
+        }
+    }
+
     var specd = spec ? spec(head, complements): {head:head,complements:complements};
     var func = obj[specd.head];
+
     for (var i = 0; i < adjuncts.length; i++) {
-        func = adjuncts[i](func);
+        var oldFunc = func;
+        var adj;
+        if (is_string(adjuncts[i])) {
+            adj = oldFunc[adjuncts[i]];
+
+            if (adjunct_args[i]) {
+                adj = adj.apply(oldFunc, adjunct_args[i]);
+            }
+        } else {
+            adj = adjuncts[i];
+        }
+        func = adj(func);
+
+        for (var key in oldFunc) {
+            if (!(key in func)) {
+                func[key] = oldFunc[key];
+            }
+        }
     }
     return func.apply(obj, specd.complements);
 };
@@ -12,7 +40,7 @@ function add(a, b) {
     return a + b;
 };
 
-function except(n) {
+Array.prototype.map.except = function(n) {
     return function(func) {
         return function() {
             return func.apply(this.filter(function(x) { return x != n; }), arguments);
@@ -20,7 +48,7 @@ function except(n) {
     };
 };
 
-function butNot(n) {
+Array.prototype.map.but_not = function(n) {
     return function(func) {
         return function(f) {
             return func.call(this, function(x) {
@@ -34,11 +62,23 @@ function butNot(n) {
     };
 };
 
-function each(head, complements) {
+Array.prototype.each = function(head, complements) {
     return {
         head: "map",
         complements: [function(x) {
             return head.apply(null, [x].concat(complements));
         }]
+    };
+};
+
+function lt(a, b) {
+    return a - b;
+};
+
+function by(compFun) {
+    return function(func) {
+        return function(optCompFun) {
+            func.call(this, compFun);
+        };
     };
 };

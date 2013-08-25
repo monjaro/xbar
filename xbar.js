eded01d25dfc9e4da536af6e76c30e5d1bdfea97
Jeremy Johnson <j@point-free.org>
2013-08-25 15:37:07 -0700

    Put specifier and adjunct into own objects

--- xbar.js
+++ xbar.js
@@ -4,10 +4,11 @@ function is_string(s) {
 
 function xbar(obj, spec, spec_args, head, complements, adjuncts, adjunct_args) {
     if (is_string(spec)) {
-        if (!(spec in obj && obj[spec])) {
+        if (!(obj.__specifiers && obj.__specifiers[spec])) {
             throw "No such specifier \"" + spec + "\" on " + obj;
         }
-        spec = obj[spec];
+
+        spec = obj.__specifiers[spec];
 
         if (spec_args) {
             spec = spec.apply(obj, spec_args);
@@ -25,10 +26,11 @@ function xbar(obj, spec, spec_args, head, complements, adjuncts, adjunct_args) {
         var oldField = field;
         var adj;
         if (is_string(adjuncts[i])) {
-            if (!(adjuncts[i] in oldField && oldField[adjuncts[i]])) {
+            if (!(oldField.__adjuncts && oldField.__adjuncts[adjuncts[i]])) {
                throw "No such adjunct \"" + adjuncts[i] + "\" on " + oldField;
             }
-            adj = oldField[adjuncts[i]];
+
+            adj = oldField.__adjuncts[adjuncts[i]];
 
             if (adjunct_args[i]) {
                 adj = adj.apply(oldField, adjunct_args[i]);
@@ -52,155 +54,155 @@ function xbar(obj, spec, spec_args, head, complements, adjuncts, adjunct_args) {
     }
 };
 
-function add(a, b) {
-    return a + b;
-};
-
-Array.prototype.map.except = function(n) {
-    return function(func) {
-        return function() {
-            return func.apply(this.filter(function(x) { return x != n; }), arguments);
+Array.prototype.map.__adjuncts = {
+    except: function(n) {
+        return function(func) {
+            return function() {
+                return func.apply(this.filter(function(x) { return x != n; }), arguments);
+            };
         };
-    };
-};
-
-Array.prototype.map.but_not = function(n) {
-    return function(func) {
-        return function(f) {
-            return func.call(this, function(x) {
-                if (x == n) {
-                    return x;
-                } else {
-                    return f(x);
-                }
-            });
-        };
-    };
-};
-
-Array.prototype.each = function(head, complements) {
-    var func;
-    if (is_string(head)) {
-        func = function(x) {
-            return x[head].apply(x, complements);
-        };
-    } else {
-        func = function(x) {
-            return head.apply(null, [x].concat(complements));
+    },
+    but_not: function(n) {
+        return function(func) {
+            return function(f) {
+                return func.call(this, function(x) {
+                    if (x == n) {
+                        return x;
+                    } else {
+                        return f(x);
+                    }
+                });
+            };
         };
     }
-
-    return {
-        field: this.map,
-        complements: [func]
-    };
 };
 
-Array.prototype.the = function(acc) {
-    function where(qualifier) {
-        return function(func) {
+Array.prototype.__specifiers = {
+    each: function(head, complements) {
+        var func;
+        if (is_string(head)) {
+            func = function(x) {
+                return x[head].apply(x, complements);
+            };
+        } else {
+            func = function(x) {
+                return head.apply(null, [x].concat(complements));
+            };
+        }
+
+        return {
+            field: this.map,
+            complements: [func]
+        };
+    },
+    the: function(acc) {
+        function where(qualifier) {
+            return function(func) {
+                return function(options) {
+                    var new_qualifier = qualifier;
+                    if (options.qualifier) {
+                        var qual = options.qualifier;
+                        new_qualifier = function(x) {
+                            return qualifier(x) && qual(x);
+                        };
+                    }
+                    options.qualifier = new_qualifier;
+                    return func.call(this, options);
+                };
+            };
+        };
+
+        function appearing_last(func) {
             return function(options) {
-                var new_qualifier = qualifier;
-                if (options.qualifier) {
-                    new_qualifier = function(x) {
-                        return qualifier(x) && options.qualifier(x);
-                    };
-                }
-                options.qualifier = new_qualifier;
+                options.last = true;
                 return func.call(this, options);
             };
         };
-    };
-
-    function appearing_last(func) {
-        return function(options) {
-            options.last = true;
-            return func.call(this, options);
+        function appearing_first(func) {
+            return function(options) {
+                options.last = false;
+                return func.call(this, options);
+            };
         };
-    };
-    function appearing_first(func) {
-        return function(options) {
-            options.last = false;
-            return func.call(this, options);
-        };
-    };
 
-    function testQual(options, item) {
-        return !options.qualifier || options.qualifier(item);
-    }
+        function testQual(options, item) {
+            return !options.qualifier || options.qualifier(item);
+        }
 
-    var selectors = {
-        largest: function(options) {
-            var largest = -Infinity;
-            for (var i = 0; i < this.length; i++) {
-                if (testQual(options, this[i])) {
-                    if (this[i] > largest) {
-                        largest = this[i];
-                    }
-                }
-            }
-            return largest;
-        },
-        smallest: function(options) {
-            var smallest = Infinity;
-            for (var i = 0; i < this.length; i++) {
-                if (testQual(options, this[i])) {
-                    if (this[i] < smallest) {
-                        smallest = this[i];
-                    }
-                }
-            }
-            return smallest;
-        },
-        element: function(options) {
-            if (options.last) {
-                for (var i = this.length - 1; i >= 0; i--) {
-                    if (testQual(options, this[i])) {
-                        return this[i];
-                    }
-                }
-            } else {
+        var selectors = {
+            largest: function(options) {
+                var largest = -Infinity;
                 for (var i = 0; i < this.length; i++) {
                     if (testQual(options, this[i])) {
-                        return this[i];
+                        if (this[i] > largest) {
+                            largest = this[i];
+                        }
                     }
                 }
-            }
-            return undefined;
-        },
-        average: function(options) {
-            var sum = 0;
-            var count = 0;
-            for (var i = 0; i < this.length; i++) {
-                if (testQual(options, this[i])) {
-                    sum += this[i];
-                    count++;
+                return largest;
+            },
+            smallest: function(options) {
+                var smallest = Infinity;
+                for (var i = 0; i < this.length; i++) {
+                    if (testQual(options, this[i])) {
+                        if (this[i] < smallest) {
+                            smallest = this[i];
+                        }
+                    }
                 }
+                return smallest;
+            },
+            element: function(options) {
+                if (options.last) {
+                    for (var i = this.length - 1; i >= 0; i--) {
+                        if (testQual(options, this[i])) {
+                            return this[i];
+                        }
+                    }
+                } else {
+                    for (var i = 0; i < this.length; i++) {
+                        if (testQual(options, this[i])) {
+                            return this[i];
+                        }
+                    }
+                }
+                return undefined;
+            },
+            average: function(options) {
+                var sum = 0;
+                var count = 0;
+                for (var i = 0; i < this.length; i++) {
+                    if (testQual(options, this[i])) {
+                        sum += this[i];
+                        count++;
+                    }
+                }
+                return sum / count;
             }
-            return sum / count;
-        }
-    };
-    for(var sel in selectors) {
-        selectors[sel].where = where;
-    }
-    selectors.element.appearing_last = appearing_last;
-    selectors.element.appearing_first = appearing_first;
-
-    if (is_string(acc)) {
-        return {field: selectors[acc], complements: [{}]};
-    }
-};
-
-function lt(a, b) {
-    return a - b;
-};
-
-Array.prototype.sort.by = function(compFun) {
-    return function(func) {
-        return function(optCompFun) {
-            func.call(this, compFun);
         };
-    };
+        for(var sel in selectors) {
+            selectors[sel].__adjuncts = {
+                "where": where
+            };
+        }
+        selectors.element.__adjuncts.appearing_last = appearing_last;
+        selectors.element.__adjuncts.appearing_first = appearing_first;
+
+        if (is_string(acc)) {
+            return {field: selectors[acc], complements: [{}]};
+        }
+        return undefined;
+    }
+};
+
+Array.prototype.sort.__adjuncts = {
+    by: function(compFun) {
+        return function(func) {
+            return function(optCompFun) {
+                func.call(this, compFun);
+            };
+        };
+    }
 };
 
 function first_wait(time) {
@@ -224,26 +226,26 @@ function never(head) {
         };
     };
 
-    function removeComplements(f) {
-        f.unless = undefined;
-        f.when = undefined;
+    function remove_adjuncts(f) {
+        f.__adjuncts = undefined;
         return f;
     };
 
-    cancel.unless = function(cond) {
-        return function(func) {
-            return removeComplements(function() {
-                func.call(this, cond);
-            });
-        };
-    };
-
-    cancel.when = function(cond) {
-        return function(func) {
-            return removeComplements(function() {
-                func.call(this, function() { return !cond(); });
-            });
-        };
+    cancel.__adjuncts = {
+        unless: function(cond) {
+            return function(func) {
+                return remove_adjuncts(function() {
+                    func.call(this, cond);
+                });
+            };
+        },
+        when: function(cond) {
+            return function(func) {
+                return remove_adjuncts(function() {
+                    func.call(this, function() { return !cond(); });
+                });
+            };
+        }
     };
 
     return {field: cancel, complements: []};
